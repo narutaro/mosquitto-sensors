ba9605d54500fe8e0b02cac9adf29ec3c493d689
narutaro <narusan@mac.com>
2020-07-12 04:34:13 +0000

    more

--- update-sas.sh
+++ update-sas.sh
@@ -1,2 +1,2 @@
-# cron refleshes the sas token. you must be able to run az command on the host
+# cron refleshes the sas token every 60min. you must be able to run az command on the host
 az iot hub generate-sas-token -n hub-apac-tokyo | jq -r .sas > sas.txt
